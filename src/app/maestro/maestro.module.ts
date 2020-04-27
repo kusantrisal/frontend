3832ca2479e3bfb83a66b715819634429cab663d
kusant <kushwantrisal@gmail.com>
2020-04-27 13:11:27 -0400

    drop zone

--- src/app/maestro/maestro.module.ts
+++ src/app/maestro/maestro.module.ts
@@ -10,14 +10,18 @@ import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { ImageComponent } from './media/image/image.component';
+import { VideoComponent } from './media/video/video.component';
+import { NgxDropzoneModule } from 'ngx-dropzone';
 
 @NgModule({
-  declarations: [ProfileComponent, ResourceComponent],
+  declarations: [ProfileComponent, ResourceComponent, ImageComponent, VideoComponent],
   imports: [
     CommonModule,
     MaterialModule,
     FlexLayoutModule,
     ReactiveFormsModule,
+    NgxDropzoneModule,
     FormsModule,
     NgxsModule.forRoot([
       MemberState
---
+++ src/app/maestro/media/image/image.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.css']
+})
+export class ImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  files: File[] = [];
+
+	onSelect(event) {
+		console.log(event);
+		this.files.push(...event.addedFiles);
+	}
+
+	onRemove(event) {
+		console.log(event);
+		this.files.splice(this.files.indexOf(event), 1);
+  }
+  
+}
